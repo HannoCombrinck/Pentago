21a58d16cc333bea41bc0cbc42dc4955a2a70dac
Hanno Combrinck <hcombrinck@gmail.com>
2019-05-08 17:11:28 +0200

    Added initial empty Match contexts.

---
+++ Assets/Pentago/Scripts/Game/Match/Contexts/IMatchContext.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class IMatchContext : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+
+    }
+
+    
+}
---
+++ Assets/Pentago/Scripts/Game/Match/Contexts/MatchHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MatchHandler : MonoBehaviour
+{
+    public IMatchContext activeContext;
+
+    private IMatchContext contextToSwitchTo;
+
+    void Awake()
+    {
+
+    }
+}
